ef5e4038073baec77e35d3a04b569aab000b7e23
Taurus Xin <457375479@qq.com>
2020-08-19 16:30:26 +0800

    formated files

--- src/components/empty.vue
+++ src/components/empty.vue
@@ -1,13 +1,13 @@
 <template>
-    <span>请从左侧选择对应的版本</span>
+  <span>请从左侧选择对应的版本</span>
 </template>
 
 <script>
-    export default {
-        name: "empty"
-    }
+export default {
+  name: 'empty',
+}
 </script>
 
 <style scoped>
-
+    
 </style>
